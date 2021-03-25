69b70c378e95311b9c283d2e2504cdc10dbaaeb6
MatGalos <67073319+MatGalos@users.noreply.github.com>
2021-03-25 10:23:17 +0100

    temp change for lab2

---
+++ Lab2/src/index.ts
@@ -0,0 +1,9 @@
+import{Snake} from './Snake';
+import{Horse} from './Horse';
+import {Animal} from './Animal';
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+  
+sam.move();
+tom.move(34);
