43c64d4febd0fee191d3e174411fd0bb998aa8da
MatGalos <67073319+MatGalos@users.noreply.github.com>
2021-06-08 19:38:59 +0200

    Lab 3 working

--- Lab3/src/Cell.ts
+++ Lab3/src/Cell.ts
@@ -1,24 +1,42 @@
-export default class Cell{
+export default class Cell {
+    cellId: string;
     cellValue: number;
     htmlElement: HTMLElement;
-    constructor(cell: HTMLElement) {
+    rowPos: number;
+    colPos: number;
+    constructor(cell: HTMLElement, rowPos: number, colPos: number) {
         this.htmlElement = cell;
+        this.rowPos = rowPos;
+        this.colPos = colPos;
+        this.cellId = `${rowPos}${colPos}`
     }
-    
-    setCellValue(value: number):boolean {
-        if(this.cellValue === 1 || this.cellValue === -1)
+
+    setCellValue(value: number): boolean {
+        if (this.cellValue === 1 || this.cellValue === -1)
             return false;
         this.cellValue = value;
-        switch(this.cellValue){
-            case -1:{
+        this.setCellInnerHtml();
+        return true;
+    }
+    refreshCellValue(value: number): void {
+        this.cellValue = value;
+        this.setCellInnerHtml();
+    }
+
+    setCellInnerHtml(): void {
+        switch (this.cellValue) {
+            case -1: {
                 this.htmlElement.innerHTML = "O";
                 break;
             }
-            case 1:{
+            case 1: {
                 this.htmlElement.innerHTML = "X";
                 break;
             }
+            default: {
+                this.htmlElement.innerHTML = "";
+                break;
+            }
         }
-        return true;
     }
-    }
+}
